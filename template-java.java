3b831b6ffa840ba50f7278c170177c0290db4496
Martin Weber Nissle <martin.webernissle@pax.ch>
2019-12-17 17:49:15 +0100

    added java template

---
+++ template-java.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright 2017 Pax, Schweizerische Lebensversicherungs-Gesellschaft AG
+ *
+ *  Licensed under the Pax License, Version 1.0 (the "License");
+ */  
+package ch.pax.xxxx;
+
+import java.util.Collection;
+
+public class Bsp {
+    private Boolean state;
+    private String message;
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(final Boolean state) {
+        this.state = state;
+    }
+}
